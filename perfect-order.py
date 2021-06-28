cf07b9d9e4bc2435ef797a77b6706a7b83e27d83
Kyle Barthelmes <kbart2401@gmail.com>
2021-06-28 18:06:27 -0400

    renamed file

--- perfect-order.py
+++ perfect-order.py
@@ -8,7 +8,8 @@ def print_menu(menu, array):
     dishes = []
     for i in range(len(array)):
         dishes.append(menu[array[i]])
-    print(dishes)
+    print('The dishes you could order that add up to your target price include:')
+    print(', '.join(dishes))
 
 
 def combo_lookup(menu, target):
@@ -21,7 +22,7 @@ def combo_lookup(menu, target):
     print("There is no combination of dishes that is equal to the target price")
 
 
-# Program init
+#### Program init ####
 try:
     cli_arg = sys.argv[1]
     file = open(cli_arg, "r")
