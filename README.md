c6e35dee9571169429f3d95ec0069e60ea7bab16
Kyle Barthelmes <kbart2401@gmail.com>
2021-06-28 20:22:07 -0400

    added README and some comments

---
+++ README.md
@@ -0,0 +1,26 @@
+# Perfect Order Program
+This program is designed to give you the "perfect order" which is an order at a restaurant with a user-defined menu and price point as well as a user-defined target price. By running the program the user will receive a list of items from the menu whose prices add up to the exact amount of the target price. If there is no perfect order avaialable a message will indicate such.
+
+## Usage
+To run the program, you must have Python 3 installed locally. From your CLI run 
+```bash
+python perfect-order.py <path to json file>
+```
+You will need a json-formatted file to run the program. An example.json is given in the repo. The format is as follows:
+
+```javascript
+{
+  "target_price": 15.05,
+  "menu": {
+    "mixed_fruit": 2.15,
+    "french_fries": 2.75,
+    "side_salad": 3.35,
+    "hot_wings": 3.55,
+    "mozzarella_sticks": 4.20,
+    "sampler_plate": 5.80,
+    "something_cheap": 0.15
+  }
+}
+```
+
+If no argument is included in the CLI a message will notify the user. 
--- perfect-order.py
+++ perfect-order.py
@@ -3,7 +3,7 @@ import json
 import math
 from itertools import combinations
 
-
+# Receives array from combo_lookup and finds the respective dishes and prints to console
 def print_menu(menu, array):
     dishes = []
     for i in range(len(array)):
@@ -11,7 +11,7 @@ def print_menu(menu, array):
     print('The dishes you could order that add up to your target price include:')
     print(', '.join(dishes))
 
-
+# Receives menu prices, runs combinations and checks for one with correct price
 def combo_lookup(menu, target):
     prices = list(menu.keys())
     for i in range(1, len(prices) + 1):
@@ -23,6 +23,7 @@ def combo_lookup(menu, target):
 
 
 #### Program init ####
+# Parse data, return message if error
 try:
     cli_arg = sys.argv[1]
     file = open(cli_arg, "r")
@@ -33,4 +34,5 @@ try:
         menu[obj["menu"][dish]] = dish
     combo_lookup(menu, target_price)
 except:
-    print('Please include an argument')
+    print('''Please include an argument. If you have included one, please ensure 
+it is a properly json-formatted document following the correct menu format''')
